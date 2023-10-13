f608a82f1c6572bfa07f2d49b234e2f50442105f
augusto <augustosaaude@gmail.com>
2023-10-13 14:40:29 -0300

    feat: remove javascript to height

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 import headerMenu from './module/shared/header/header-menu-hamburguer.js';
 headerMenu();
-import styleHeightMain from './module/shared/style-height-main/style-height-main.js';
 
 headerMenu();
-styleHeightMain();
