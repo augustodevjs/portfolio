fb8aa59fdd56b527d3dce451fce7c5a2258970bc
augusto <augustosaaude@gmail.com>
2023-10-14 12:45:23 -0300

    feat: update

--- js/module/shared/header/header-active.js
+++ js/module/shared/header/header-active.js
@@ -1,18 +1,11 @@
 export default function headerActive() {
-  // Log initial values for debugging
-  console.log('URL:', window.location.href);
-  
   const url = window.location.href;
   const match = url.match(/\/([^\/]+)\/?$/);
   const links = document.querySelectorAll('.menu-right nav ul li');
 
-  // Log values to check if the script is reaching this point
-  console.log('Match:', match);
-  console.log('Links:', links);
-
   if (match && match[1]) {
     switch (match[1]) {
-      case 'https://jaugustodev.netlify.app/':
+      case 'jaugustodev.netlify.app':
         links[0].classList.add('active');
         break;
       case 'projetos':
@@ -22,13 +15,5 @@ export default function headerActive() {
         links[2].classList.add('active');
         break;
     }
-  } else {
-    // Log a message if there's an issue with the match
-    console.error('Error: Unable to match the URL.');
-  }
+  } 
 }
-
-// Run the script after the DOM has fully loaded
-document.addEventListener('DOMContentLoaded', function() {
-  headerActive();
-});
