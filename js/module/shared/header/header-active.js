9477ac35cb5cf7be75c1a52c55a615895c3b787b
augusto <augustosaaude@gmail.com>
2023-10-14 12:30:55 -0300

    update

--- js/module/shared/header/header-active.js
+++ js/module/shared/header/header-active.js
@@ -1,7 +1,14 @@
 export default function headerActive() {
+  // Log initial values for debugging
+  console.log('URL:', window.location.href);
+  
   const url = window.location.href;
   const match = url.match(/\/([^\/]+)\.html$/);
   const links = document.querySelectorAll('.menu-right nav ul li');
+
+  // Log values to check if the script is reaching this point
+  console.log('Match:', match);
+  console.log('Links:', links);
 
   if (match && match[1]) {
     switch (match[1]) {
@@ -15,5 +22,13 @@ export default function headerActive() {
         links[2].classList.add('active');
         break;
     }
+  } else {
+    // Log a message if there's an issue with the match
+    console.error('Error: Unable to match the URL.');
   }
 }
+
+// Run the script after the DOM has fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+  headerActive();
+});
