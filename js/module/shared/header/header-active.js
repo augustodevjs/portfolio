a8dff8001367124ee11b7a4c1e1d31a61d099e6b
augusto <augustosaaude@gmail.com>
2023-10-14 12:24:21 -0300

    feat: add header-active

---
+++ js/module/shared/header/header-active.js
@@ -0,0 +1,17 @@
+export default function headerActive() {
+  const url = window.location.href;
+  const match = url.match(/\/([^\/]+)\.html$/);
+  const links = document.querySelectorAll('.menu-right nav ul li');
+
+  switch(match[1]) {
+    case 'home':
+      links[0].classList.add('active');
+      break;
+    case 'projetos':
+      links[1].classList.add('active');
+      break;
+    case 'contato':
+      links[2].classList.add('active');
+      break;
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import animation from './module/shared/animation/animation.js';
+import headerActive from './module/shared/header/header-active.js';
 import headerMenu from './module/shared/header/header-menu-hamburguer.js';
 
 animation();
 headerMenu();
+headerActive();
