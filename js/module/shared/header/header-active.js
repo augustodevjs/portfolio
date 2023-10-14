86a24b3f8f3ca39dc49ebbc9fa2fba6804c7e427
augusto <augustosaaude@gmail.com>
2023-10-14 12:34:44 -0300

    feat: update

--- js/module/shared/header/header-active.js
+++ js/module/shared/header/header-active.js
@@ -12,7 +12,7 @@ export default function headerActive() {
 
   if (match && match[1]) {
     switch (match[1]) {
-      case 'home':
+      case '/':
         links[0].classList.add('active');
         break;
       case 'projetos':
