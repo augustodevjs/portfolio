6060368dcb49837d63bcd666b50bd1226ecf5932
augusto <augustosaaude@gmail.com>
2023-11-19 12:09:09 -0300

    feat: add video of intranet

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-import animation from './module/shared/animation/animation.js';
-import headerActive from './module/shared/header/header-active.js';
-import headerMenu from './module/shared/header/header-menu-hamburguer.js';
+import video from './module/video/video.js';
+import animation from './module/animation/animation.js';
+import headerActive from './module/header/header-active.js';
+import headerMenu from './module/header/header-menu-hamburguer.js';
 
+video();
 animation();
 headerMenu();
 headerActive();
