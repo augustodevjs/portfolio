831e0334b96059bc425ca8c1c83266c71e101cf4
augusto <augustosaaude@gmail.com>
2023-11-28 19:57:26 -0300

    feat: create firsts steps of header in portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
+import './styles/global.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Poppins } from 'next/font/google'
+import { Header } from './components/header/header'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700', '800', '900'], display: 'swap' })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <main className="container">
+          <Header />
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
