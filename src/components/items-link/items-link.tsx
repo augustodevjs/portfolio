512888ae990c7f914f85c636636f96e97a223c5c
augusto <augustosaaude@gmail.com>
2023-12-03 11:49:56 -0300

    feat: fix style in route header nav

--- src/components/items-link/items-link.tsx
+++ src/components/items-link/items-link.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import Link from "next/link"
 import { ItemsLinkProps } from "../types"
+import { usePathname } from "next/navigation"
 
 export const ItemsLink = ({ label, href }: ItemsLinkProps) => {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
   return (
     <li>
-      <Link href={href}>{label}</Link>
+      <Link className={isActive ? 'active' : ''} href={href}>{label}</Link>
     </li>
   )
 }
