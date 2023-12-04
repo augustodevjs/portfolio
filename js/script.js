4c2aab0c44b6cf05f5921b7160012756cc74725a
augusto <augustosaaude@gmail.com>
2023-12-04 19:14:28 -0300

    feat: remove all files

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// import video from './module/video/video.js';
-import animation from './module/animation/animation.js';
-import headerActive from './module/header/header-active.js';
-import headerMenu from './module/header/header-menu-hamburguer.js';
-
-// video();
-animation();
-headerMenu();
-headerActive();
