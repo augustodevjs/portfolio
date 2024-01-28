8e2fd5d00621045f13dca4521a9e414b6bc6354e
augusto <augustosaaude@gmail.com>
2024-01-28 12:33:04 -0300

    feat: change icon

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -10,6 +10,8 @@ export const metadata: Metadata = {
 export default async function Page() {
   const { project: ProjetoPage } = await ProjetoService();
 
+  console.log(ProjetoPage)
+
   return (
     <Projects project={ProjetoPage} />
   )
